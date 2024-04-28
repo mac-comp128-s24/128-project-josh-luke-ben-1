ce424c640d0d0418023d2d71db23aed69ee3913b
bking0521 <benking0521@gmail.com>
2024-04-28 18:49:38 -0500

    update

--- src/Wordle/Word.java
+++ src/Wordle/Word.java
@@ -1,17 +1,35 @@
 package Wordle;
 
-import edu.macalester.graphics.Path;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Word {
+    private List<String> words;
+
+    public Word() {
+        words = new ArrayList<>();
+    }
+
+    public void readWordsFromFile() {
+        try {
+            words = Files.readAllLines(Paths.get("C:\\Users\\benki\\OneDrive\\Documents\\GitHub\\128-project-josh-luke-ben-1\\src\\Wordle\\valid-wordle-words.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getRandomWord() {
+        Random rand = new Random();
+        return words.get(rand.nextInt(words.size()));
+    }
+
     
-    public Word(){
-
-    }
-
-    public String pickWord(){
-        
-
-        return "";
-    }
-
 }
--- src/Wordle/Wordle.java
+++ src/Wordle/Wordle.java
@@ -1,5 +1,9 @@
 package Wordle;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.Color;
+
 import edu.macalester.graphics.CanvasWindow;
 
 public class Wordle {
@@ -9,15 +13,50 @@ public class Wordle {
 
     Background background;
     CanvasWindow canvas;
+    String randomWord;
 
     public Wordle(){
-        CanvasWindow canvas = new CanvasWindow("Wordle", CANVAS_WIDTH, CANVAS_HEIGHT);
+        canvas = new CanvasWindow("Wordle", CANVAS_WIDTH, CANVAS_HEIGHT);
         background = new Background(canvas);
 
+        Word word = new Word();
+        randomWord = word.getRandomWord();
+
+        if (background.getGuesses() == 6){
+            background.loser(randomWord);
+        }
+
+        if (randomWord.equals(background.getEnteredWord())){
+            background.winner(randomWord);
+        }   
+
+        List<Color> colors = compareWords(background.getEnteredWord(), randomWord);
+        background.fillBoxes(colors);
+
+        canvas.draw();
     }
 
-    public void run(){
+    public List<Color> compareWords(String enteredWord, String randomWord) {
+    List<Color> colors = new ArrayList<>();
+    for (int i = 0; i < enteredWord.length(); i++) {
+        char enteredLetter = enteredWord.charAt(i);
+        char randomLetter = randomWord.charAt(i);
+        if (enteredLetter == randomLetter) {
+            colors.add(Color.GREEN);
+        } else if (randomWord.indexOf(enteredLetter) != -1) {
+            colors.add(Color.YELLOW);
+        } else {
+            colors.add(Color.GRAY);
+        }
+    }
+    return colors;
+}
+
+    public String getRandomWord() {
+        return randomWord;
+    }
+
+    public static void main(String[] args) {
         Wordle wordle = new Wordle();
     }
-
 }

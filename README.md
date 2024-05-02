265e7e5d8b3012a2864c29301ba0c6124a567642
josh-planko41 <jplanko@macalester.edu>
2024-05-01 23:36:17 -0500

    txt files completed and tests written

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# Project Title Goes Here
+# Data Structures Game Hub: Connect Four and Wordle
 
-Info about your project goes here
+This project builds upon Ben and Josh's Comp-127 project by building a second iteration to the game hub they designed in the previous semester.
+This game hub creates Connect Four and Wordle games along with a central hub wher you can choose which game you want to play by typing in the name of the game. 
+
+Built by Ben King, Josh Planko, and Luke Moore for the Comp 128 Spring Semseter Final project in April of 2024. 
--- test/CFVisualTester.java
+++ test/CFVisualTester.java
@@ -1,3 +1,6 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 import ConnectFour.Board;
@@ -12,5 +15,41 @@ public class CFVisualTester {
     public void CFInterfaceTest(){
         CanvasWindow canvas = new CanvasWindow("Connect Four!", CANVAS_WIDTH, CANVAS_HEIGHT);
         new Board(canvas);
+    }
+
+    @Test
+    public void winConditionTest(){
+        CanvasWindow canvas = new CanvasWindow("Connect Four!", CANVAS_WIDTH, CANVAS_HEIGHT);
+        Board board = new Board(canvas);
+        char[][] grid = board.getBoardArray();
+        grid[0][0] = 'R';
+        grid[0][1] = 'R';
+        grid[0][2] = 'R';
+        grid[0][3] = 'R';
+        assertTrue(board.isWinner('R', grid));
+        char[][] grid1 = board.getBoardArray();
+        grid1[0][0] = 'R';
+        grid1[1][0] = 'R';
+        grid1[2][0] = 'R';
+        grid1[3][0] = 'R';
+        assertTrue(board.isWinner('R', grid1));
+        char[][] grid2 = board.getBoardArray();
+        grid2[0][0] = 'Y';
+        grid2[1][1] = 'Y';
+        grid2[2][2] = 'Y';
+        grid2[3][3] = 'Y';
+        assertTrue(board.isWinner('Y', grid2));
+        char[][] grid3 = board.getBoardArray();
+        grid3[0][4] = 'Y';
+        grid3[1][3] = 'Y';
+        grid3[2][2] = 'Y';
+        grid3[3][1] = 'Y';
+        assertTrue(board.isWinner('Y', grid3));
+        char[][] grid4 = new char[6][7];
+        grid4[0][0] = 'R';
+        grid4[0][1] = 'R';
+        grid4[0][2] = 'Y';
+        grid4[0][3] = 'R';
+        assertFalse(board.isWinner('Y', grid4));
     }
 }
